23d6d7bf34d44a03640853de14e646f4fd0d6ee8
Kashishdafe <kashishdafe0410@gmail.com>
2024-11-06 13:53:33 +0530

    changes

---
+++ src/test/java/com/example/kanakagalekarCA/KanakagalekarCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanakagalekarCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanakagalekarCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
